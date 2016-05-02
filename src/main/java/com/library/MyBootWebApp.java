ddb3deacd3066c098d00dbbab43f4c2b17ccb2f8
Vitor Nunes Benedito <vitorbenedito@Vitors-MacBook-Pro.local>
2016-05-02 15:08:27 -0300

    Test

--- src/main/java/com/library/MyBootWebApp.java
+++ src/main/java/com/library/MyBootWebApp.java
@@ -3,6 +3,7 @@ package com.library;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
+
 public class MyBootWebApp extends SpringBootServletInitializer {
     @Override protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
--- src/main/java/com/library/MyConfiguration.java
+++ src/main/java/com/library/MyConfiguration.java
@@ -1,12 +1,22 @@
 package com.library;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@ComponentScan
 public class MyConfiguration {
 
     @Bean
@@ -21,5 +31,17 @@ public class MyConfiguration {
             		.exposedHeaders("Authorization", "Content-Type");
             }
         };
+    }
+    
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        return DataSourceBuilder
+            .create()
+            .username("devmute")
+            .password("devmute#33")
+            .url("jdbc:mysql://devmute.cwh2lv5bp1ns.sa-east-1.rds.amazonaws.com:3306/library")
+            .driverClassName("com.mysql.jdbc.Driver")
+            .build();
     }
 }
